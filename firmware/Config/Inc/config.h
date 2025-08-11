4f8ea191e132f585beae69ef45e33cf9aeac39eb
lakshay <lakshay45gupta@gmail.com>
2025-08-11 21:07:00 +0000

    pragma and config + extra fix for if no .vscode

---
+++ firmware/Config/Inc/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//--------------------------------------------------------------------------------
+// Battery Pack layout
+#ifndef NUM_BATTERY_MODULES
+#define NUM_BATTERY_MODULES             8      // Number of battery modules per VoltTemp
+#endif
+
+#ifndef NUM_TEMPERATURE_SENSORS
+#define NUM_TEMPERATURE_SENSORS         4      // Number of temperature sensors per VoltTemp
+#endif
