99ca6e313625958d0a50fd501fc3d5d989d5559f
lakshay <lakshay45gupta@gmail.com>
2025-08-13 04:23:36 +0000

    fixed makefile for running tests

---
+++ firmware/config/Src/configMsgs.c
@@ -0,0 +1,10 @@
+#include "common.h"
+#include "config.h"
+#include "pinConfig.h"
+
+#define PRE(s)  "\r    "s"  "   // \r removes the filepath and 'note: '#pragma message:...' parts
+#define STR(x)  #x
+#define XSTR(x) STR(x)
+
+
+#pragma message(PRE("⚡") "NUM_BATTERY_MODULES set to                " XSTR(NUM_BATTERY_MODULES))
